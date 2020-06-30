c51388641576a70c28a14e5d193277201cc1a4e0
Erick-34 <erickoliveira2060@gmail.com>
2020-06-30 10:00:43 -0300

    done

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/views/index.html')
-})
+//ROUTES
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/about.html')
-})
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
 
-app.get('/works', (req, res) => {
-    res.sendFile(__dirname + '/views/work.html')
-})
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/work.html");
+});
 
-app.get('/photos', (req, res) => {
-   res.sendFile(__dirname + '/views/photo.html')
-})
+app.get("/photos", (req, res) => {
+  res.sendFile(__dirname + "/views/photo.html");
+});
 
+//Server
 app.listen(3000, () => {
-    console.log("Server on in https://localhost:3000")
-})
+  console.log("Server on in https://localhost:3000");
+});
